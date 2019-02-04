64e9ed1243570597490684a90ba3e5e5acfdd01b
TheoBerglin <theoberglin@outlook.com>
2019-02-04 21:43:30 +0100

    10 triangles calculation (#12)
    * test file for BU implemented
    
    * Structure of the triangles file is set
    
    * Works for Binary undirected
    
    * Test for binary directed added
    
    * Updated the test file
    
    * Done with binary directed
    
    * Tolerance added to test
    
    * Works for Weighted undirected
    
    * Added test for weighted directed
    
    * Done with negative undirected graphs
    
    * Done with triangles
    
    * Second approach, same algorithm for all undirected and all directed
    
    * Done with triangle calculations
    
    * Small changes
    
    * Small changes
    
    * Fixed test so they work for the convention of weighted graphs
    
    * Works for weighted directed, weighted undirected and binary directed
    
    * Works for negative undirected graphs
    
    * Done with weighted directed negative graphs
    
    * Correct implementation of negative weights
    
    * Added some explanatory comments
    
    * I've made only small changes to the comments.
    
    Should the references be added also to the comments of the function triangles?
    
    % Reference on calculations:
    % * J.P.Onnela et al. "Intensity and coherence of motifs in weighted complex networks"
    % * G.Fagiolo, "Clustering in complex directed networks"
    % * Negative matrix: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3931641/
    
    * I've added the references to the comments:
    
    % Reference on calculations:
    % * J.P.Onnela et al. "Intensity and coherence of motifs in weighted complex networks"
    % * G.Fagiolo, "Clustering in complex directed networks"
    % * Negative matrix: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3931641/

---
+++ graph/triangles.m
@@ -0,0 +1,62 @@
+function t = triangles( A, type )
+% TRIANGLES Number of triangles around a node
+%
+% T = TRIANGLES(A, TYPE) calculates the number of triangles T
+%   around all nodes in the graph represented by the adjacency matrix A.
+%   TYPE specifies the type of graph:
+%   Graph.BD = Binary Directed
+%   Graph.BU = Binary Undirected
+%   Graph.WD = Weighted Directed
+%   Graph.WU = Weighted Undirected
+%   Graph.WDN = Weighted Directed with Negative weights
+%   Graph.WUN = Weighted Undirected with Negative weights
+%
+% For binary graphs, the number of triangles are the number of pairs of
+% neighbours of a node that are connected with each other.  
+% For weighted graphs, the number of triangles is defined as the geometric
+% mean of the weights forming the triangles. In directed graphs, a triangle
+% is considered only if the directions between the three nodes (vertices of
+% the triangle) are arranged as a cycle i.e. each one of the nodes has one
+% incoming and one outgoing edge. Thus, each set of 3 nodes contributes to
+% 1 triangle. Negative weights will be handled in the same way as positive
+% weights, i.e, two negative connections will result in a positive triangle
+% while one or three negative connections will result in a negative
+% triangle.
+%
+% In these calculations, the diagonal of the connection matrix is removed,
+% i.e. self-connections are not considered. 
+%
+% Reference on calculations: 
+% * J.P.Onnela et al. "Intensity and coherence of motifs in weighted complex networks"
+% * G.Fagiolo, "Clustering in complex directed networks"
+% * Negative matrix: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3931641/
+%
+% Authors: Adam Liberda, Theo Berglin, Mite Mijalkov, Ehsan Kakaei, Giovanni Volpe
+% Date: 2019/01/31
+% http://braph.org/
+
+A = remove_diagonal(A);
+N = size(A,2); % Number of nodes
+
+if type == Graph.BU || type == Graph.WU || type == Graph.WUN
+    A13 = abs(A).^(1/3).*sign(A); % No imaginary numbers for negative matrix, Binary matrix won't change
+    A3 = A13^3;
+    t = diag(A3)'/2;
+    
+else % if type == Graph.BD || type == Graph.WD || type == Graph.WDN
+    t = zeros(1,N);
+    W = abs(A).^(1/3).*sign(A); % No imaginary numbers for negative matrix, Binary matrix won't change
+    for u = 1:1:N
+        nodesout = find(W(u,:));
+        nodesin = find(W(:,u))';
+        if ~isempty(nodesout) && ~isempty(nodesin)
+            % We have a triangle if one of the out nodes goes to the one of the in nodes
+            % u--> out-node --> in-node --> u
+            temp_num = W(u,nodesout)*W(nodesout,nodesin)*W(nodesin,u);
+            t(u) = sum(sum(temp_num));
+        end
+    end
+end
+
+end
+
