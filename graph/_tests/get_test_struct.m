e8e709bf7abe7b10d870846f2f6abac0174a1af2
Adam Liberda <liberda.a@gmail.com>
2019-02-20 10:47:38 +0100

    Bug fix in get_test_struct.

--- graph/_tests/get_test_struct.m
+++ graph/_tests/get_test_struct.m
@@ -1,13 +1,20 @@
-function test_struct = get_test_struct(connectivity, type, exp_result, name)
+function test_struct = get_test_struct(connectivity, type, exp_result, name, community_structure)
 % GET_TEST_STRUCT A structure containing information needed for a test
 
-% TEST_STRUCT = GET_TEST_STRUCT(C0NNECTIVITY, TYPE, EXPRESULT, NAME) Given
-% the properties the function returns a struct of the parameters.
-% CONNECTIVITY is the graph for which the test should be carried out on
-% and TYPE being the type of graph. EXPRESULT is the expected result.
-% NAME is the name of the test.
+% TEST_STRUCT = GET_TEST_STRUCT(C0NNECTIVITY, TYPE, EXPRESULT, NAME, COMMUNITY_STRUCTURE) 
+%   Given the properties the function returns a struct of the parameters.
+%   CONNECTIVITY is the graph for which the test should be carried out on
+%   and TYPE being the type of graph. EXPRESULT is the expected result.
+%   NAME is the name of the test. COMMUNITY_STRUCTURE is an optional
+%   parameter that contains a community structure corresponding to the
+%   connectivity matrix.
+
+if nargin == 4  % no community structure passed
+    community_structure = [];
+end
 
 test_struct = struct('connectivity', connectivity, 'type', type, ...
-    'exp_result', exp_result, 'name', name, 'community_structure', [], 'passed', false);
+    'exp_result', exp_result, 'name', name, 'community_structure',...
+    community_structure, 'passed', false);
 end
 
