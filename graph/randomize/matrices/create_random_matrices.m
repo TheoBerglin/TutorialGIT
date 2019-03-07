e63849e022fee87e60e7cd9639f08e39e3617eaa
AdamLiberda <31771637+AdamLiberda@users.noreply.github.com>
2019-03-07 14:05:07 +0100

    151 standard matrices (#168)
    * create random matrices function created
    
    * Created some binary directed and undirected graphs
    
    * Fixed minor bug + generated matrices for WD and WU of different densities
    
    * Changed folder structure
    
    * First half of 1000 nodes matrices
    
    * Second half of 1000 node matrices
    
    * Small changes

--- graph/randomize/matrices/create_random_matrices.m
+++ graph/randomize/matrices/create_random_matrices.m
@@ -1,41 +1,46 @@
 clear all, clc, close all
+densities = [0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7];
+
 %% Settings
-dens = 0.15; % Between 0 and 1.
-nodes = 100;
+nodes = 1000;
 s = nodes*nodes;
-type = Graph.BU;
+type = Graph.WU;
 directed = Graph.is_directed(type);
 binary = Graph.is_binary(type);
-%dens = dens+0.01; % To account for diagonal
-%% Generate matrix
-indizes = randperm(s, round(s*dens));
-A = zeros(nodes, nodes);
-A(indizes) = 1;
 
-%% Binary/weight settings
-if binary
-    str_bin = 'bin';
-else
-    % Add weights
-    str_bin = 'wei';
-    W = rand(s);
-    A = A.*W;
+for i=1:length(densities)
+    dens = densities(i); % Between 0 and 1.
+    %dens = dens+0.01; % To account for diagonal
+    %% Generate matrix
+    indizes = randperm(s, round(s*dens));
+    A = zeros(nodes, nodes);
+    A(indizes) = 1;
+    
+    %% Binary/weight settings
+    if binary
+        str_bin = 'bin';
+    else
+        % Add weights
+        str_bin = 'wei';
+        W = rand(nodes);
+        A = A.*W;
+    end
+    
+    %% Directed/undirected settings
+    if directed
+        str_dir = 'dir';
+    else
+        %Symmetrize if undirected
+        str_dir = 'undir';
+        A = triu(A);
+        A = A+A.';
+    end
+    A = replace_diagonal(A, 1);
+    dA =  density(A, type)/100;
+    fprintf('Created matrix of density: %.4f\n',dA);
+    %% Save matrix
+    current_loc = fileparts(which('create_random_matrices.m'));
+    file_name = sprintf('dens_%.3f_nodes_%d_%s_%s.txt', dens, nodes, str_bin, str_dir);
+    save_file = sprintf('%s%snodes_%d%s%s', current_loc, filesep, nodes, filesep, file_name);
+    save(save_file, 'A','-ascii', '-double', '-tabs')
 end
-
-%% Directed/undirected settings
-if directed
-    str_dir = 'dir';
-else
-    %Symmetrize if undirected
-    str_dir = 'undir';
-    A = triu(A);
-    A = A+A.';
-end
-A = replace_diagonal(A, 1);
-dA =  density(A, type)/100;
-fprintf('Created matrix of density: %.4f\n',dA);
-%% Save matrix
-current_loc = fileparts(which('create_random_matrices.m'));
-file_name = sprintf('dens_%.3f_nodes_%d_%s_%s.txt', dens, nodes, str_bin, str_dir);
-save_file = sprintf('%s%s%s', current_loc, filesep, file_name);
-save(save_file, 'A','-ascii', '-double', '-tabs')
