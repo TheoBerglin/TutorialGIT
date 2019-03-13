1b5ea706d8daa82d8703086a2df316f8cc4995b8
Adam Liberda <liberda.a@gmail.com>
2019-03-13 11:18:41 +0100

    Added input parameter checks + impl. function for random_rewiring.

--- graph/randomize/randomize_functions/randm_giovanni_bd.m
+++ graph/randomize/randomize_functions/randm_giovanni_bd.m
@@ -1,21 +1,19 @@
 function B = randm_giovanni_bd(A,I,error)
-% RANDM_BD calculates a random binary directed matrix
+% RANDM_GIOVANNI_BD calculates a random binary directed matrix
 %
 % B = RANDM_BD(A) calculates a random binary directed matrix
 %   preserving the indegree and outdegree of each node.
 %   Therefore, also the indegree and outdegree distributions are preserved.
 %
-% [B,MW] = RANDM_BD(A,I,ERROR) permits one to set
+% B = RANDM_BD(A,I,ERROR) permits one to set
 %   the maximum number of iterations I (default I=100) and
 %   the maximum fraction of miswired edges ERROR (default ERROR = 1e-4,
 %   i.e. at most one edge out of 10000 is miswired).
-%   MW is the number of miswired edges that are eliminated.
 %
 % Conditions on the input connectivity matrix A:
 %   (1) A is square
 %   (2) A(r,c) = 0 or 1 (binary)
 %   (3) A(r,r) = 0 (no self-connection)
-%   These conditions are NOT enforced or checked.
 %
 % Notes on the algorithm:
 %   Almost all edges are rewired at once. This implies that some edges
@@ -24,12 +22,16 @@ function B = randm_giovanni_bd(A,I,error)
 %   The algorithm iteratively rewires these miswired edges
 %   until the maximum number of iterations (I) or the acceptable maximum
 %   fraction of miswired edges (number of edges * ERROR) is reached.
-%   At the end, the miswired edges are eliminated.
+%   At the end, the miswired edges are randomly rewired to a available node.
 %
 % See also randm_bu, randm_wd, randm_wu.
 
-% Author: Giovanni Volpe
-% Date: 2016/04/01
+% Version 1:
+%   - Author: Giovanni Volpe
+%   - Date: 2016/04/01
+% Version 2:
+%   - Authors: Adam Liberda, Theo Berglin
+%   - Date: 2019/03/13
 
 % maximum number of iterations
 if nargin<2 || isempty(I)
@@ -41,8 +43,23 @@ if nargin<3 || isempty(error)
     error = 1e-4;
 end
 
+% check squareness
+if ~isequal(size(A,1), size(A,2))
+    error('Input matrix needs to be square');
+end
+
+% check binarism
+if ~all(all(A == 0 | A == 1))
+    error('Input matrix is not binary');
+end
+
 % number of nodes
 N = length(A);
+
+% check for self-connections and remove these if they exist
+if ~all(A(1:N+1:end) == 0)
+    A = remove_diagonal(A);
+end
 
 % find edge indexes
 e = find(A); % e = (c-1)*N+r
@@ -70,32 +87,12 @@ for i = 1:1:I
     end
 end
 
-% eliminate remaining miswired edges
+% find remaining miswired edges and their lin. index in A
 ind_mw = miswired();
 ind_rm = e_orig(ind_mw);
 
-for i = 1 : length(ind_rm)
-    [~, rm_col] = ind2sub(size(A),ind_rm(i));  % find old row and col
-    occupied = r(c == rm_col);  % find occupied rows that connect to col
-    possible = setdiff(1:1:N, occupied);  % get possible out-nodes
-    idx = randperm(length(possible), 1);  % pick one index
-    rm_row_new = possible(idx);  % get new row index
-    r(ind_mw(i)) = rm_row_new;  % update the miswired row to the new one
-end
-    
-% Below lines of coded are no longer needed since miswired edges are taken
-% care of in the above code snippet.
-% e(ind_mw) = 0;
-% c(ind_mw) = 0;
-% r(ind_mw) = 0;
+random_rewiring(ind_rm)
 
-% ind = find(e~=0); 
-% e = e(ind);
-% c = c(ind);
-% r = r(ind);
-
-% number of miswired edges
-% mw = length(ind_mw);
 
     function permutation(ind)
         % permutes the edges with indices ind
@@ -131,17 +128,28 @@ end
         end
     end
 
+    function random_rewiring(ind)
+        % rewires the row (out-going) index of the edges specified by ind 
+        % to a randomly chosen available row (node). 
+        
+        % this random rewiring doesn't work for dense matrices, since it looks for
+        % nodes that doesn't have an edge to the node in question, and in dense
+        % matrices a node can have an in-degree equal to N-1, hence no available nodes.
+        for i = 1 : length(ind)
+            [~, rm_col] = ind2sub(size(A),ind(i));  % find old row and col of miswired edge
+            occupied = r(c == rm_col);  % find occupied rows that connect to col
+            possible = setdiff(1:1:N, occupied);  % get possible out-nodes
+            rm_row_new = rm_col;
+            while isequal(rm_row_new, rm_col)  % to not create a self-connection
+                idx = randperm(length(possible), 1);  % pick one index
+                rm_row_new = possible(idx);  % get new row index
+            end
+            r(ind_mw(i)) = rm_row_new;  % update the miswired row to the new one
+        end
+    end
+
 % construct the connectivity matrix B with rewired edges
 B = zeros(size(A));
 B(sub2ind(size(B),r,c)) = 1;
-
-% for i = 1 : length(ind_rm)
-%     [~, rm_j] = ind2sub(size(A),ind_rm(i));
-%     
-%     possible = find(B(:, rm_j) == 0);  % Find possible nodes to rewire to
-%     idx = randperm(length(possible), 1);  % Pick one index
-%     rm_i_new = possible(idx);
-%     B(rm_i_new, rm_j) = 1;
-% end
 
 end
