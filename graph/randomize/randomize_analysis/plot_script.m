22695a9313ea41af7a8fbfbb530148fb5fbd4bc4
TheoBerglin <theoberglin@outlook.com>
2019-03-14 10:12:05 +0100

    Plot script finished

---
+++ graph/randomize/randomize_analysis/plot_script.m
@@ -0,0 +1,25 @@
+clear all, close all, clc;
+%% Settings
+file1 = 'speed_null_model_dir_sign';
+file2 = 'speed_randomize_gio_bct_dir_sign';
+data_files = {file1, file2};
+density = 0.1;
+
+figure()
+for i = 1:length(data_files)
+    d = load(data_files{i});
+    speed_data = d.speed_data;
+    dens_data = extractfield(speed_data, 'density');
+    indices = find(dens_data == density);
+    time_data = extractfield(speed_data, 'time');
+    time_data = time_data(indices);
+    size_data = extractfield(speed_data, 'size');
+    size_data = size_data(indices);
+    name = replace(data_files{i}, 'speed_', '');
+    name = replace(name, '_', '-');
+    plot_data(i) = struct('name', name, 'size_data', size_data, 'time_data', time_data);
+    loglog(plot_data(i).size_data, plot_data(i).time_data, 'DisplayName', plot_data(i).name)
+    hold on
+end
+
+legend()
