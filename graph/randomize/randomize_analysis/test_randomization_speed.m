1bbfae3e32dc969378e77f665b60fe7c69170b12
TheoBerglin <theoberglin@outlook.com>
2019-03-25 08:36:24 +0100

    BCT U speed test. Index out of bounds error

--- graph/randomize/randomize_analysis/test_randomization_speed.m
+++ graph/randomize/randomize_analysis/test_randomization_speed.m
@@ -1,8 +1,8 @@
 clear all, clc, close all
 %% Settings
-func = 'randomize_bct_D';
+func = 'randomize_bct_U';
 densities = [0.01 0.1 0.2 0.5 0.7];%[0.01 0.02 0.03 0.04 0.05 0.1, 0.2 0.3 0.4 0.5 0.6 0.7];
-sizes = [50 60 100 150 200]; % 400 800];% 1000 2000];
+sizes = [50 60 100 150 200];  %400 800];% 1000 2000];
 n_randomizations = 40;
 dir = true;
 wei = false;
@@ -32,18 +32,25 @@ fprintf('Running the randomization speed test for:\n %s\n', func);
 for di = 1:length(densities)
     for si = 1:length(sizes)
         d = densities(di); % Density
-        s = sizes(si); % Size of matrix
-        time = 0;
+        s = sizes(si); % Size of matrix2
         A = create_matrix(d, s, dir, wei);
-        for i = 1:n_randomizations
-            tic
-            eval(sprintf('%s(A);', func))
-            time = time + toc;
+        try
+            time = 0;
+            for i = 1:n_randomizations
+                tic
+                eval(sprintf('%s(A);', func))
+                time = time + toc;
+            end
+            speed_data(speed_ind) = struct('size', s, 'density', d,...
+                'n_randomizations', n_randomizations, 'time', time/n_randomizations, ...
+                'weighted', wei, 'directed', dir);
+            fprintf('Size: %d Density: %.2f Time per run: %.7f\n', s, d, time/n_randomizations)
+        catch
+            speed_data(speed_ind) = struct('size', s, 'density', d,...
+                'n_randomizations', NaN, 'time', NaN, ...
+                'weighted', wei, 'directed', dir);
+            fprintf('ERROROR: Size: %d Density: %.2f did not finish\n', s, d)
         end
-        speed_data(speed_ind) = struct('size', s, 'density', d,...
-            'n_randomizations', n_randomizations, 'time', time/n_randomizations, ...
-            'weighted', wei, 'directed', dir);
-        fprintf('Size: %d Density: %.2f Time per run: %.7f\n', s, d, time/n_randomizations)
         speed_ind = speed_ind + 1;
     end
 end
