d3da597bde7d73b37ba831dd5ea8e6759cc8f5cc
AdamLiberda <31771637+AdamLiberda@users.noreply.github.com>
2019-03-28 11:24:21 +0100

    192 create matrix fix (#197)
    * Accounted for diagonal + changed implementation of creating symmetric matrices
    
    * Some comments
    
    * Fixed bug
    
    * Generated new matrices for all type size 10

--- graph/randomize/matrices/create_random_matrices.m
+++ graph/randomize/matrices/create_random_matrices.m
@@ -1,21 +1,42 @@
 clear all, clc, close all
 
-densities = [0.01 0.02 0.03 0.04 0.06 0.07 0.08 0.09 0.1 0.15 0.2 0.3 0.4 0.5];
+densities = [0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.15 0.2 0.3 0.4 0.5 0.6 0.7];
 %% Settings
-nodes = 100;
+nodes = 10;
 s = nodes*nodes;
+possible_connections = nodes*(nodes-1);
 type = Graph.WU;
 directed = Graph.is_directed(type);
 binary = Graph.is_binary(type);
 
 for i=1:length(densities)
     dens = densities(i); % Between 0 and 1.
-    %dens = dens+0.01; % To account for diagonal
+
     %% Generate matrix
-    indizes = randperm(s, round(s*dens));
     A = zeros(nodes, nodes);
-    A(indizes) = 1;
     
+    if directed
+        % generate edges as long as desired density is not met
+        while numel(find(A)) ~= round(possible_connections*dens)
+            new_edges = round(possible_connections*dens) - numel(find(A));
+            indices = randperm(s, new_edges);
+            A(indices) = 1;
+            A = remove_diagonal(A);
+        end
+    else % undirected
+        % generate edges as long as (half of the) desired density is not met
+        while numel(find(A)) ~= round(possible_connections*dens/2)
+            new_edges = round(possible_connections*dens/2) - numel(find(A));
+            indices = randperm(s, new_edges);
+            if ~isempty(indices)
+                [row, col] = ind2sub(size(A), indices);
+                indices_to_keep = col>row;  % fill upper triangle
+                A(indices(indices_to_keep)) = 1;
+                A = remove_diagonal(A);
+            end
+        end
+    end
+
     %% Binary/weight settings
     if binary
         str_bin = 'bin';
@@ -27,18 +48,16 @@ for i=1:length(densities)
     end
     
     %% Directed/undirected settings
-    if directed
+    if directed 
         str_dir = 'dir';
     else
-        %Symmetrize if undirected
         str_dir = 'undir';
-        A = triu(A);
-        A = A+A.';
+        A = A+A.';  % symmetrize
     end
-    A = replace_diagonal(A, 1);
+        
+    %% Save matrix
     dA =  density(A, type)/100;
     fprintf('Created matrix of density: %.4f\n',dA);
-    %% Save matrix
     current_loc = fileparts(which('create_random_matrices.m'));
     file_name = sprintf('dens_%.3f_nodes_%d_%s_%s.txt', dens, nodes, str_bin, str_dir);
     save_file = sprintf('%s%snodes_%d%s%s', current_loc, filesep, nodes, filesep, file_name);
