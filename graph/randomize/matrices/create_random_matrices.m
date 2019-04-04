db7a7102584cb464397a9b32e69500e131ce7191
TheoBerglin <theoberglin@outlook.com>
2019-04-04 09:48:58 +0200

    New matrices

--- graph/randomize/matrices/create_random_matrices.m
+++ graph/randomize/matrices/create_random_matrices.m
@@ -1,11 +1,11 @@
 clear all, clc, close all
 
-densities = [0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.15 0.2 0.3 0.4 0.5 0.6 0.7];
+densities = [0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1]+0.005;
 %% Settings
 nodes = 50;
 s = nodes*nodes;
 possible_connections = nodes*(nodes-1);
-type = Graph.BU;
+type = Graph.WU;
 threshold = 0.3;
 directed = Graph.is_directed(type);
 binary = Graph.is_binary(type);
