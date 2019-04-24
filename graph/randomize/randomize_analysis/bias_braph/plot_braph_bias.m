d3f812e5fcdfdfb84c3eca25d02e5e487d84cec8
Adam Liberda <liberda.a@gmail.com>
2019-04-24 13:39:08 +0200

    Miswire plot for BD used in report

--- graph/randomize/randomize_analysis/bias_braph/plot_braph_bias.m
+++ graph/randomize/randomize_analysis/bias_braph/plot_braph_bias.m
@@ -1,5 +1,5 @@
 clear all, close all;
-file_name = 'bias_braph_randomize_braph_BU_bias_fix.mat';
+file_name = 'bias_braph_randomize_braph_BD_bias.mat';
 nodes = 50;
 data = load(file_name);
 data = data.data;
@@ -8,29 +8,29 @@ for i = 1:length(data)
         data = data(i).node_data;
         mw = zeros(1, length(data));
         dens = zeros(1, length(data));
-        rw = zeros(1, length(data));
-        rw_nnz = zeros(1, length(data));
+%         rw = zeros(1, length(data));
+%         rw_nnz = zeros(1, length(data));
         for j=1:length(data)
-            mw(j) = mean(data(j).miswires);
-            rw(j) = mean(data(j).random_wires);
-            rw_nnz(j) = nnz(data(j).random_wires);
+            mw(j) = nnz(data(j).miswires)/length(data(j).miswires);
+%             rw(j) = mean(data(j).random_wires);
+%             rw_nnz(j) = nnz(data(j).random_wires);
             dens(j) = data(j).density;
         end
     end
 end
-
-figure()
-plot(dens, rw_nnz);
-xlabel('Density')
-ylabel('Number of events of odd cycles')
-title('500 runs')
-figure()
-plot(dens, rw);
-xlabel('Density')
-ylabel('Mean number of times we correct miswires')
-title('500 runs')
+% 
+% figure()
+% plot(dens, rw_nnz);
+% xlabel('Density')
+% ylabel('Number of events of odd cycles')
+% title('500 runs')
+% figure()
+% plot(dens, rw);
+% xlabel('Density')
+% ylabel('Mean number of times we correct miswires')
+% title('500 runs')
 figure()
 plot(dens, mw);
 xlabel('Density')
-ylabel('Mean number of miswires after randomization')
-title('500 runs')
+ylabel('Fraction of miswire events after first rewiring')
+title('Binary directed, size:50x50, number of randomizations: 500')
